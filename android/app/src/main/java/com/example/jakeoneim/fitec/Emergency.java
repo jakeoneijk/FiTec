ade3117c7445b0bd9bb30fef3c94fc56622f343c
OHHYEONJEONG <guswjd3927@naver.com>
2019-01-31 14:14:56 -0500

    connecting bluetooth android

--- android/app/src/main/java/com/example/jakeoneim/fitec/Emergency.java
+++ android/app/src/main/java/com/example/jakeoneim/fitec/Emergency.java
@@ -1,16 +1,129 @@
 package com.example.jakeoneim.fitec;
 
 import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
 import android.content.Intent;
 import android.net.Uri;
+import android.os.Bundle;
 import android.telephony.SmsManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
+import com.example.jakeoneim.fitec.BluetoothSPP;
+//import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
 
 //1 is falling down, 2 is heart failure
 public class Emergency extends Activity{
     private String call_number = "17657015379";
     private String[] neibor_numbers = {"17657015379","17655864549"};
+    private BluetoothSPP bt;
+    boolean emergencycheck;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_emergency);
+        emergencycheck = getIntent().getBooleanExtra("emergency",true);//emergency체크
 
+
+
+        //******************bluetooth communication*****************
+        bt = new BluetoothSPP(this);
+
+        if (!bt.isBluetoothAvailable()) { //블루투스 사용 불가
+            Toast.makeText(getApplicationContext()
+                    , "Bluetooth is not available"
+                    , Toast.LENGTH_SHORT).show();
+            finish();
+        }
+        /*bt.setOnDataReceivedListener(new BluetoothSPP.OnDataReceivedListener() { //데이터 수신
+            public void onDataReceived(byte[] data, String message) {
+                Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+            }
+        });*/
+        bt.setAutoConnectionListener(new BluetoothSPP.AutoConnectionListener() {
+            @Override
+            public void onAutoConnectionStarted() {
+                send();
+            }
+
+            @Override
+            public void onNewConnection(String name, String address) {
+                send();
+            }
+        });
+        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() { //연결됐을 때
+            public void onDeviceConnected(String name, String address) {
+                Toast.makeText(getApplicationContext()
+                        , "Connected to " + name + "\n" + address
+                        , Toast.LENGTH_SHORT).show();
+                send();
+            }
+
+            public void onDeviceDisconnected() { //연결해제
+                Toast.makeText(getApplicationContext()
+                        , "Connection lost", Toast.LENGTH_SHORT).show();
+            }
+
+            public void onDeviceConnectionFailed() { //연결실패
+                Toast.makeText(getApplicationContext()
+                        , "Unable to connect", Toast.LENGTH_SHORT).show();
+            }
+        });
+        Button btnConnect = findViewById(R.id.connectButton); //연결시도
+        btnConnect.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                if (bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
+                    bt.disconnect();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), DeviceList.class);
+                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
+                }
+            }
+        });
+        String Address = "00:14:03:05:59:2C";
+        bt.autoConnect(Address);
+    }
+    public void onStart() {
+        super.onStart();
+        if (!bt.isBluetoothEnabled()) {
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
+        } else {
+            if (!bt.isServiceAvailable()) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_OTHER); //DEVICE_ANDROID는 안드로이드 기기 끼리
+            }
+        }
+    }
+    public void send(){//부저 울림 메소드
+        bt.send("emergency", true);
+    }
+    public void onDestroy() {
+        super.onDestroy();
+        bt.stopService(); //블루투스 중지
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if (resultCode == Activity.RESULT_OK)
+                bt.connect(data);
+        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_OTHER);
+            } else {
+                Toast.makeText(getApplicationContext()
+                        , "Bluetooth was not enabled."
+                        , Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+    //****************************************************************************여기까지 블루투스 통신
 
     public void sendMessage(int typeofemergency){
         int temp = 0;
--- android/app/src/main/java/com/example/jakeoneim/fitec/MainActivity.java
+++ android/app/src/main/java/com/example/jakeoneim/fitec/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.jakeoneim.fitec;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,7 +12,6 @@ public class MainActivity extends AppCompatActivity {
     FallDownCheck fTest;
     HeartRateCheck hTest;
     boolean isConnected;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -19,8 +20,8 @@ public class MainActivity extends AppCompatActivity {
         fTest = new FallDownCheck(); // fallDown Object create
         hTest = new HeartRateCheck();// heartBeat Object create
         isConnected = false;
-
-        findViewById(R.id.connectButton).setOnClickListener(new View.OnClickListener() {
+        Button connectBtn = (Button)findViewById(R.id.connectButton);
+        connectBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 if(!isConnected){
@@ -35,9 +36,13 @@ public class MainActivity extends AppCompatActivity {
                 }
             }
         });
-
     }
-
+    /*emergency발생시를 버튼클릭으로 일단 대체*/
+    public void emergencyClick(View v) {
+        Intent intent = new Intent(MainActivity.this, Emergency.class);
+        intent.putExtra("emergency", true);
+        startActivity(intent);
+    }
     public boolean makeConnectionWithSensor(){
         //isConnected should be changed here
         isConnected = true;
@@ -57,11 +62,9 @@ public class MainActivity extends AppCompatActivity {
             if(hTest.isProblem(data[0])){
                 hTest.start();
             }
-
             if(fTest.isFallenDown(data[1],data[2],data[3],data[4],data[5],data[6])){
                 fTest.start();
             }
-
         }
 
 
