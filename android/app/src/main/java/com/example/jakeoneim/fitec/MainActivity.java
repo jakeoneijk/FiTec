8515583f117184d260eccfa20d89487753e54456
OHHYEONJEONG <guswjd3927@naver.com>
2019-01-31 15:10:43 -0500

    complete connecting android and bluetooth

--- android/app/src/main/java/com/example/jakeoneim/fitec/MainActivity.java
+++ android/app/src/main/java/com/example/jakeoneim/fitec/MainActivity.java
@@ -1,5 +1,8 @@
 package com.example.jakeoneim.fitec;
 
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
@@ -7,42 +10,112 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import app.akexorcist.bluetotohspp.library.BluetoothSPP;
+import app.akexorcist.bluetotohspp.library.BluetoothState;
+import app.akexorcist.bluetotohspp.library.DeviceList;
 
+public class MainActivity extends AppCompatActivity {
+    public static Context context;
     FallDownCheck fTest;
     HeartRateCheck hTest;
     boolean isConnected;
+    public static BluetoothSPP bt;
+
+    Emergency eg ;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        fTest = new FallDownCheck(); // fallDown Object create
-        hTest = new HeartRateCheck();// heartBeat Object create
-        isConnected = false;
-        Button connectBtn = (Button)findViewById(R.id.connectButton);
-        connectBtn.setOnClickListener(new View.OnClickListener() {
-            @Override
+        //******************bluetooth communication*****************
+        bt = new BluetoothSPP(this);
+
+        if (!bt.isBluetoothAvailable()) { //블루투스 사용 불가
+            Toast.makeText(getApplicationContext()
+                    , "Bluetooth is not available"
+                    , Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        bt.setBluetoothConnectionListener(new BluetoothSPP.BluetoothConnectionListener() { //연결됐을 때
+            public void onDeviceConnected(String name, String address) {
+                Toast.makeText(getApplicationContext()
+                        , "Connected to " + name + "\n" + address
+                        , Toast.LENGTH_SHORT).show();
+                eg = new Emergency();
+            }
+
+            public void onDeviceDisconnected() { //연결해제
+                Toast.makeText(getApplicationContext()
+                        , "Connection lost", Toast.LENGTH_SHORT).show();
+            }
+
+            public void onDeviceConnectionFailed() { //연결실패
+                Toast.makeText(getApplicationContext()
+                        , "Unable to connect", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        Button btnConnect = findViewById(R.id.connectButton); //연결시도
+        btnConnect.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                if(!isConnected){
-                    if(makeConnectionWithSensor()){
-                        Toast.makeText(getApplicationContext(),"start FitBit",Toast.LENGTH_LONG);
-                    }else{
-                        Toast.makeText(getApplicationContext(),"connection problem",Toast.LENGTH_LONG);
-                    }
-                }else{
-                    isConnected = false;
-                    Toast.makeText(getApplicationContext(),"stop FitBit",Toast.LENGTH_LONG);
+                if (bt.getServiceState() == BluetoothState.STATE_CONNECTED) {
+                    bt.disconnect();
+                } else {
+                    Intent intent = new Intent(getApplicationContext(), DeviceList.class);
+                    startActivityForResult(intent, BluetoothState.REQUEST_CONNECT_DEVICE);
                 }
             }
         });
+        //*********************************************************************
+
+        fTest = new FallDownCheck(); // fallDown Object create
+        hTest = new HeartRateCheck();// heartBeat Object create
+        isConnected = false;
     }
+
     /*emergency발생시를 버튼클릭으로 일단 대체*/
     public void emergencyClick(View v) {
-        Intent intent = new Intent(MainActivity.this, Emergency.class);
-        intent.putExtra("emergency", true);
-        startActivity(intent);
+        eg.send();
     }
+    public void onStart() {
+        super.onStart();
+        if (!bt.isBluetoothEnabled()) { //
+            Intent intent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(intent, BluetoothState.REQUEST_ENABLE_BT);
+        } else {
+            if (!bt.isServiceAvailable()) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_OTHER); //DEVICE_ANDROID는 안드로이드 기기 끼리
+
+            }
+        }
+    }
+    public void onDestroy() {
+        super.onDestroy();
+        bt.stopService(); //블루투스 중지
+    }
+
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == BluetoothState.REQUEST_CONNECT_DEVICE) {
+            if (resultCode == Activity.RESULT_OK)
+                bt.connect(data);
+        } else if (requestCode == BluetoothState.REQUEST_ENABLE_BT) {
+            if (resultCode == Activity.RESULT_OK) {
+                bt.setupService();
+                bt.startService(BluetoothState.DEVICE_OTHER);
+
+            } else {
+                Toast.makeText(getApplicationContext()
+                        , "Bluetooth was not enabled."
+                        , Toast.LENGTH_SHORT).show();
+                finish();
+            }
+        }
+    }
+    //****************************************************************************
+
     public boolean makeConnectionWithSensor(){
         //isConnected should be changed here
         isConnected = true;
@@ -62,14 +135,10 @@ public class MainActivity extends AppCompatActivity {
             if(hTest.isProblem(data[0])){
                 hTest.start();
             }
+
             if(fTest.isFallenDown(data[1],data[2],data[3],data[4],data[5],data[6])){
                 fTest.start();
             }
         }
-
-
     }
-
-
-
 }
